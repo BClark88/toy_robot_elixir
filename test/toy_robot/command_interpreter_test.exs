750ea08b5db4cfa7a63bbb7a9e983d277e5bcb15
Brady Clark <bradyjclark88@gmail.com>
2021-01-30 14:37:45 +0800

    command interpreter

---
+++ test/toy_robot/command_interpreter_test.exs
@@ -0,0 +1,15 @@
+defmodule ToyRobot.CommandInterpreterTest do
+  use ExUnit.Case
+  doctest ToyRobot.CommandInterpreter
+  alias ToyRobot.CommandInterpreter
+
+  test "marks invalid commands as invalid" do
+    commands = ["SPIN", "TWIRL", "EXTERMINATE"]
+    output = commands |> CommandInterpreter.interpret()
+    assert output == [
+      {:invalid, "SPIN"},
+      {:invalid, "TWIRL"},
+      {:invalid, "EXTERMINATE"}
+    ]
+  end
+end
